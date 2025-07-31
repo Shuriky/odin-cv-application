3b00bb00f06add46201fbcf3b7141c1f27c083f2
Shuriky <hdoo0027@student.monash.edu>
2025-07-31 19:31:07 +1000

    add a container for document

--- repo/src/main.jsx
+++ repo/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
 import Logo from './react-components/Logo.jsx'
 import Form from './react-components/Form.jsx'
 
@@ -10,6 +9,15 @@ createRoot(document.getElementById('root')).render(
     <div class="form-section">
       <Logo />
       <Form />
+    </div>
+  </StrictMode>
+)
+
+createRoot(document.getElementById('document-section')).render(
+  <StrictMode>
+    <div id="document-container">
+      <div id="document">
+      </div>
     </div>
   </StrictMode>
 )
