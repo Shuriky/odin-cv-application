bb570ef6b73f0b2669ea4bdda81fa3964e71347f
Shuriky <hdoo0027@student.monash.edu>
2025-08-02 08:16:30 +1000

    add the print function (works but isnt scaling the content properly)

--- repo/src/App.jsx
+++ repo/src/App.jsx
@@ -2,6 +2,8 @@ import Logo from './form-components/Logo.jsx'
 import Form from './form-components/Form.jsx'
 import Document from './document-components/Document.jsx';
 import React, { useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
 
 export default function App() {
     const [formData, setFormData] = useState({
@@ -17,6 +19,9 @@ export default function App() {
         skills: [],
     });
 
+    const contentRef = useRef(null);
+    const reactToPrintFn = useReactToPrint({ contentRef });
+
     return (
         <div id="app">
             <div className="form-section">
@@ -25,8 +30,10 @@ export default function App() {
             </div>
             <div id="separate-line"></div>
             <div id="document-container">
-                <button id="printButton">Save as PDF</button>
-                <Document formData={formData} setFormData={setFormData}/>
+                <button id="printButton" onClick={reactToPrintFn}>Export Document</button>
+                <div ref={contentRef} className="print-content">
+                    <Document formData={formData} setFormData={setFormData}/>
+                </div>
             </div>
         </div>
     );
